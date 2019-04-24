9d7d19a3d26b987d8d414790819dc8331102f753
Louie Williford <dustyfingers01257@gmail.com>
2019-04-24 13:05:00 -0600

    made dice image match generated number asnd assigned to click event listener

--- 4-DOM-pig-game/starter/app.js
+++ 4-DOM-pig-game/starter/app.js
@@ -9,18 +9,31 @@ GAME RULES:
 
 */
 
-var scores, roundScore, activePlayer, dice;
+var scores, roundScore, activePlayer;
 
 scores = [0, 0];
 roundScore = 0;
-activePlayer = 1;
-dice = Math.floor(Math.random() * 6) + 1;
-
-document.querySelector('#current-' + activePlayer).textContent = dice;
-// document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
-
-
-var x = document.querySelector('#score-0').textContent;
-console.log(x);
+activePlayer = 0;
 
 document.querySelector('.dice').style.display = 'none';
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+// for show
+// document.querySelector('#current-' + activePlayer).textContent = dice;
+// document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
+// var x = document.querySelector('#score-0').textContent;
+// console.log(x);
+
+document.querySelector('.btn-roll').addEventListener('click', () => {
+  // random number
+  var dice = Math.floor(Math.random() * 6) + 1;
+  // display result
+  var diceDOM = document.querySelector('.dice');
+  diceDOM.style.display = 'block';
+  diceDOM.src = 'dice-' + dice + '.png';
+
+  // update round score if rolled number was not a 1
+
+});
